1a75d3d0febfa02cbc8c6887b893c3c20fe523eb
catversstudio <asisasastudent@gmail.com>
2026-01-19 16:47:56 +0530

    Refactor categories and posts in data.js data saved
    Updated categories and posts data with new entries and descriptions.

--- data.js
+++ data.js
@@ -8,38 +8,74 @@
 
 window.infinityDB = {
   "categories": [
-    { "id": "c1", "name": "Useful Tools" },
-    { "id": "c2", "name": "Study Sites" },
-    { "id": "c3", "name": "Photo Prompts" },
-    { "id": "cat_1768712824799", "name": "Games" },
-    { "id": "cat_1768712912648", "name": "Online Movies" },
-    { "id": "cat_1768712920066", "name": "Download Movies" },
-    { "id": "cat_1768712927652", "name": "Anime" },
-    { "id": "cat_1768712952535", "name": "Music" },
-    { "id": "cat_1768712992139", "name": "Illegal Apps" }
+    {
+      "id": "c1",
+      "name": "Useful Tools"
+    },
+    {
+      "id": "c2",
+      "name": "Study Sites"
+    },
+    {
+      "id": "c3",
+      "name": "Photo Prompts"
+    },
+    {
+      "id": "cat_1768712824799",
+      "name": "Games"
+    },
+    {
+      "id": "cat_1768712912648",
+      "name": "Online Movies"
+    },
+    {
+      "id": "cat_1768712920066",
+      "name": "Download Movies"
+    },
+    {
+      "id": "cat_1768712927652",
+      "name": "Anime"
+    },
+    {
+      "id": "cat_1768712952535",
+      "name": "Music"
+    },
+    {
+      "id": "cat_1768712992139",
+      "name": "Illegal Apps"
+    },
+    {
+      "id": "cat_1768712809616",
+      "name": "Study Sites"
+    },
+    {
+      "id": "cat_1768712817430",
+      "name": "Photo Prompt "
+    }
   ],
   "posts": [
     {
-      "id": 101,
-      "catId": "c1",
-      "title": "NoteGPT",
-      "link": "https://notegpt.io",
-      "img": "https://ui-avatars.com/api/?name=NoteGPT&background=1e293b&color=fff",
-      "desc": "AI Summary tool for students."
-    },
-    {
-      "id": 1768716313092,
+      "id": 1768726958240,
       "catId": "c2",
       "title": "NoteGPT",
       "link": "https://notegpt.io",
-      "img": "https://i.ibb.co/xKZsQmgL/download.jpg",
-      "desc": "NoteGPT is an AI-powered study platform created to help students learn faster and smarter.",
+      "img": "https://i.ibb.co/N2Jsf1Xh/Picsart-26-01-18-14-23-48-734.jpg",
+      "desc": "NoteGPT is here to make studying easier. It takes those endless videos, PDFs, articles—pretty much anything you need to read or watch—and turns them into simple, organized notes. You save time, and things just click faster. You don’t even have to sit through full YouTube videos anymore; NoteGPT pulls out the main ideas for you. It’s built with students in mind, especially if you’re in high school or college and drowning in information. Whether you’re cramming for exams or just trying to keep up, NoteGPT helps you cut through the noise and actually remember what matters.",
+      "clicks": 0
+    },
+    {
+      "id": 1768729645717,
+      "catId": "c2",
+      "title": "Topperworld",
+      "link": "https://topperworld.in",
+      "img": "https://i.ibb.co/fzym1pPG/images.png",
+      "desc": "Topperworld is an online education platform that provides free study materials for engineering and IT students. It offers subject-wise notes, previous year question papers, syllabuses, and tutorials to support university exam preparation and technical learning. The platform is built to make academic resources easily accessible, helping students save time, strengthen concepts, and study more effectively.",
       "clicks": 0
     }
   ],
   "intro": {
     "title": "Thank you for choosing our platform.",
-    "sub": "Explore a refined collection of powerful and useful websites."
+    "sub": "Explore a refined collection of powerful and useful websites.”"
   },
-  "about": "Infinity Tool is built to organize the internet into useful categories.\n\nThe goal is to make online resources easier to discover, understand, and use — all from one place."
+  "about": "Infinity Tool is built to organize the internet into useful categories. From study platforms and productivity tools to gaming, video editing, music, and entertainment websites, it helps users explore the web in a simple and structured way.\n\nThe goal is to make online resources easier to discover, understand, and use — all from one place."
 };
